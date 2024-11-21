44e0e0cccd3d562b03c58dc7a5c8e015822b9903
FlakoBIG <168838033+FlakoBIG@users.noreply.github.com>
2024-11-21 00:35:43 -0300

    Commit

--- GoldenGymApp/forms.py
+++ GoldenGymApp/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from .models import Cliente,Encargado,Plan
+from .models import Cliente,Encargado,Novedad,Reporte,Plan
 
 class ClienteForm(forms.ModelForm):
-    membresia = forms.MultipleChoiceField(
+    membresia = forms.ChoiceField(
         choices=Cliente.PLANES_MEMBRESIA,
-        widget=forms.CheckboxSelectMultiple,
+        widget=forms.RadioSelect,  # Opción para seleccionar un solo plan
         label="PLAN"
     )
 
@@ -17,7 +17,6 @@ class ClienteForm(forms.ModelForm):
             'rut': forms.TextInput(attrs={'class': 'form-control', 'style': 'background-color: #666; color: white;'}),
             'correo': forms.EmailInput(attrs={'class': 'form-control', 'style': 'background-color: #666; color: white;'}),
         }
-
 
 class EncargadoForm(forms.ModelForm):
     # Definir el formulario utilizando el modelo Encargado
@@ -40,8 +39,20 @@ class EncargadoForm(forms.ModelForm):
         # Aquí puedes agregar validaciones adicionales para el correo
         return correo
 
+class NovedadForm(forms.ModelForm):
+    class Meta:
+        model = Novedad
+        fields = ['titulo', 'contenido', 'imagen' ,'ruta_video']  # Incluye ambos campos
+
+class ReporteForm(forms.ModelForm):
+    class Meta:
+        model = Reporte
+        fields = ['fecha', 'peso_actual', 'altura', 'press_banca', 'sentadilla', 'peso_muerto']
+        widgets = {
+            'fecha': forms.SelectDateWidget(years=range(2024, 2025)),
+        }
+
 class PlanForm(forms.ModelForm):
     class Meta:
         model = Plan
         fields = ['nombre', 'precio', 'duracion_dias']
-
--- GoldenGymApp/models.py
+++ GoldenGymApp/models.py
@@ -14,7 +14,7 @@ class Cliente(models.Model):
     rut = models.CharField(max_length=12, unique=True)
     correo = models.EmailField(unique=True)
     membresia = models.CharField(max_length=10, choices=PLANES_MEMBRESIA)
-    suscripcion_activa = models.BooleanField(default=False)  # Suscripción activa
+    suscripcion_activa = models.BooleanField(default=True)  # Suscripción activa
 
     def __str__(self):
         return f"{self.nombre} {self.apellido} - {self.membresia}"
@@ -41,6 +41,39 @@ class Encargado(models.Model):
     def __str__(self):
         return f"{self.nombre} {self.apellido} ({self.usuario})"
     
+class Novedad(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha_publicacion = models.DateField(auto_now_add=True)
+    imagen = models.ImageField(upload_to='img/', blank=True, null=True)  # Usar ImageField
+    ruta_video = models.CharField(max_length=300, blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Reporte(models.Model):
+    cliente = models.ForeignKey(Cliente, related_name='reportes', on_delete=models.CASCADE)
+    fecha = models.DateField()
+    peso_actual = models.DecimalField(max_digits=5, decimal_places=2)
+    altura = models.DecimalField(max_digits=5, decimal_places=2)
+    imc_actual = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    press_banca = models.DecimalField(max_digits=5, decimal_places=2)
+    sentadilla = models.DecimalField(max_digits=5, decimal_places=2)
+    peso_muerto = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def calcular_imc(self):
+        # Calcula el IMC en base al peso y la altura.
+        if self.altura > 0:
+            return round(self.peso_actual / (self.altura ** 2), 2)
+        return None
+
+    def save(self, *args, **kwargs):
+        # Calcula el IMC antes de guardar el reporte
+        self.imc_actual = self.calcular_imc()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Reporte de {self.cliente.nombre} {self.cliente.apellido} - {self.fecha.strftime('%b %Y')}"
 
 class Plan(models.Model):
     nombre = models.CharField(max_length=50)
